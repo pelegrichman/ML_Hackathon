cd436dcf76f6c48f815b2b3955c80cdd2288316d
HeliPerlin <heliperlin@gmail.com>
2022-06-02 13:53:20 +0300

    created skeleton, rename columns and removed useless

--- preprocessing.py
+++ preprocessing.py
@@ -1,25 +1,15 @@
 import pandas as pd
 import numpy as np
 import json
-
-# import generes_data
-# import productions_data
-# import keywords_data
-# import cast_data
-# import crew_data
-# import collections_data
-
+from pandas_profiling import ProfileReport
 from itertools import chain
 from collections import Counter
-
 from sklearn.preprocessing import MultiLabelBinarizer
 
 import ast
+# TODO :
+# TODO 1 : Figure out what to do with the id column
 
-
-# _------------------------------------------------------
-# ----- HARDCODE any encoding so we can handle well new ones (ignore them)
-# _------------------------------------------------------
 
 def add_missing_encodings(encodings: pd.DataFrame, cols_names: set):
     cols = set(encodings.columns)
@@ -56,8 +46,7 @@ def list_col_name(dataframe, col):
         return np.NAN
 
 
-JSON_COLS = ['belongs_to_collection', 'genres', 'production_companies', 'production_countries',
-             'spoken_languages', 'keywords', 'cast', 'crew']
+JSON_COLS = []
 CONVERTERS = {key: parse_column for key in JSON_COLS}
 
 SEED = 13
@@ -72,48 +61,71 @@ def to_names_list(sub_df, allowed=None):
 
 
 class Preprocessing:
-    # ALLOWED_GENRES = generes_data.data
-    # BEST_PRODUCTIONS = productions_data.data
-    # KEYWORDS = keywords_data.data
-    # KNOWN_CAST = cast_data.data
-    # KNOWN_CREW = crew_data.data
-    # COLLECTION_COUNT = collections_data.data
-
-    @staticmethod
-    def generate_frequent_values(df: pd.DataFrame, col: str, thresh):
-        vals = df[col].apply(to_names_list)
-        v = pd.Series(Counter(chain.from_iterable(vals)))
-        return {ind for ind in v.index if (v > thresh)[ind]}
 
     def __init__(self, src, encoding=None):
         self.df: pd.DataFrame = pd.read_csv(src, converters=CONVERTERS, header=0, encoding=encoding)
         self.__process()
         self.df = self.df.fillna(0)
 
-    def split(self, frac):
-        pass
-
-    def save(self, path):
-        self.df.to_csv(path)
-
-    # ======================================
-    # ----------- Processing ---------------
-    # ======================================
+    ############################## Processing ##############################
 
     def __process(self):
-        self.__process_age()
-        self.__process_basic_stage()
-        self.__drop_useless()
-        self.__process_basic_stage()
-        self.__process_diagnosis_date()
-        self.__process_histological_diagnosis()
+        # change names with Hebrew letters + drop useless
+        self.__rename()
+        self.__remove_useless()
 
-    def __drop_useless(self):
-        self.df.drop(['Form Name', 'Hospital', 'User Name', ], axis=1,
+        # process the columns that stay
+        self.__process_age()
+        self.__process_basic_stage() # Heli
+        self.__process_Her2() # Asif
+        self.__process_histological_diagnosis() # Peleg - done
+        self.__process_histological_degree() # Peleg
+        self.__process_ivi_lymphovascular_invasion()
+        self.__process_KI67_protein()
+        self.__process_meta_mark()
+        self.__process_margin_type()
+        self.__process_side()
+        self.__process_stage()
+        self.__process_surgery_name1()
+        self.__process_surgery_sum()
+        self.__process_tumor_mark()
+        self.__process_tumor_depth()
+        self.__process_er_pr()
+        self.__process_actual_activity()
+
+    def __rename(self):
+        self.df.rename(columns=
+                  {'אבחנה-Age': 'Age',
+                   'אבחנה-Basic stage': 'Basic stage',
+                   'אבחנה-Her2': 'Her2',
+                   'אבחנה-Histological diagnosis': 'Histological diagnosis',
+                   'אבחנה-Histopatological degree': 'Histopatological degree',
+                   'אבחנה-Ivi -Lymphovascular invasion': 'Ivi -Lymphovascular invasion',
+                   'אבחנה-KI67 protein': 'KI67 protein',
+                   'אבחנה-M -metastases mark (TNM)': 'M -metastases mark (TNM)',
+                   'אבחנה-Margin Type': 'Margin Type',
+                   'אבחנה-Side': 'Side',
+                   'אבחנה-Stage': 'Stage',
+                   'אבחנה-Surgery name1': 'Surgery name1',
+                   'אבחנה-Surgery sum': 'Surgery sum',
+                   'אבחנה-T -Tumor mark (TNM)': 'T -Tumor mark (TNM)',
+                   'אבחנה-Tumor depth': 'Tumor depth',
+                   'אבחנה-er': 'er',
+                   'אבחנה-pr': 'pr',
+                   }, inplace=True)
+
+    def __remove_useless(self):
+        self.df.drop(['Form Name', 'Hospital', 'User Name',
+                      'אבחנה-Diagnosis date', 'אבחנה-Lymphatic penetration', 'אבחנה-Lymphatic penetration',
+                      'אבחנה-N -lymph nodes mark (TNM)', 'אבחנה-Nodes exam',
+                      'אבחנה-Positive nodes', 'אבחנה-Surgery date1', 'אבחנה-Surgery date2',
+                      'אבחנה-Surgery date3', 'אבחנה-Surgery name2', 'אבחנה-Surgery name3',
+                      'אבחנה-Tumor width', 'surgery before or after-Activity date',
+                      ''], axis=1,
                      inplace=True)
 
-    def __process_age(self):
-        self.df = self.df[self.df['Age'] > 0] # TODO: check the age range
+    def __process_age(self): # TODO: remove?
+        self.df = self.df[self.df['Age'] > 0]
         self.df = self.df[self.df['Age'] < 110]
 
     def __process_basic_stage(self):
@@ -121,98 +133,59 @@ class Preprocessing:
         self.df = pd.get_dummies(self.df, columns=['Basic Stage'],
                                  drop_first=True)
 
-    def __process_diagnosis_date(self): # TODO - implement
+    def __process_Her2(self):
         pass
 
-    def __process_histological_diagnosis(self): # TODO - implement: parse
-        # the data (values can be composed of different types), then convert
-        # to categorial type
+    def __process_histological_diagnosis(self):
         pass
 
+    def __process_histological_degree(self):
+        pass
 
+    def __process_ivi_lymphovascular_invasion(self):
+        pass
 
-    ###############################  DOR ########################################
+    def __process_KI67_protein(self):
+        pass
 
-    def __prepare_status(self):
-        self.df['is_released'] = (self.df['status'] == 'Released').astype(int)
+    def __process_meta_mark(self):
+        pass
 
-    def __prepare_tagline(self):
-        length = self.df['tagline'].str.len()
-        self.df['is_short_tagline'] = ~(length.isna()) & (length < 120)
+    def __process_margin_type(self):
+        pass
 
-    def __prepare_keywords(self):
-        mlb = MultiLabelBinarizer()
-        keywords_list = lambda x: to_names_list(x, Preprocessing.KEYWORDS)
-        keywords_encoding = pd.DataFrame(mlb.fit_transform(self.df['keywords'].apply(keywords_list)),
-                                         columns=mlb.classes_, index=self.df.index)
-        keywords_encoding = add_missing_encodings(keywords_encoding, Preprocessing.KEYWORDS)
-        self.df = self.df.join(keywords_encoding)
+    def __process_side(self):
+        pass
 
-    def __prepare_genres(self):
-        mlb = MultiLabelBinarizer()
-        genres_list = lambda x: to_names_list(x, Preprocessing.ALLOWED_GENRES)
-        generes_encoding = pd.DataFrame(mlb.fit_transform(self.df['genres'].apply(genres_list)), columns=mlb.classes_,
-                                        index=self.df.index)
-        generes_encoding = add_missing_encodings(generes_encoding, Preprocessing.ALLOWED_GENRES)
-        self.df = self.df.join(generes_encoding)
-        self.df.drop(['genres'], axis=1, inplace=True)
+    def __process_stage(self):
+        pass
 
-    def __prepare_belongs_to_collection(self):
-        self.df['belongs_to_collection'] = self.df['belongs_to_collection'].apply(
-            lambda x: get_val(x, 'id', first=True))
-        self.df['collection_count'] = self.df['belongs_to_collection'].apply(
-            lambda x: get_val(Preprocessing.COLLECTION_COUNT, x, default=0))
-        self.df.drop(['belongs_to_collection'], axis=1, inplace=True)
+    def __process_surgery_name1(self):
+        pass
 
-    def __prepare_original_language(self):
-        # maybe drop completely (A LOT in english others not so much)
-        self.df['originally_english'] = (self.df['original_language'] == 'en').astype(int)
+    def __process_surgery_sum(self):
+        pass
 
-    def __prepare_production_companies(self):
-        mlb = MultiLabelBinarizer()
-        productions_list = lambda x: to_names_list(x, Preprocessing.BEST_PRODUCTIONS)
-        productions_encoding = pd.DataFrame(mlb.fit_transform(self.df['production_companies'].apply(productions_list)),
-                                            columns=mlb.classes_, index=self.df.index)
-        productions_encoding = add_missing_encodings(productions_encoding, Preprocessing.BEST_PRODUCTIONS)
-        self.df = self.df.join(productions_encoding)
+    def __process_tumor_mark(self):
+        pass
 
-    def __prepare_date(self):
-        release_date = pd.to_datetime(self.df['release_date'], dayfirst=True, errors='coerce')
-        self.df['release_month'] = release_date.dt.month
+    def __process_tumor_depth(self):
+        pass
 
-        today = pd.to_datetime('today')
-        weeks_delta = (today - release_date) / np.timedelta64(1, 'W')
-        years_delta = (today - release_date) / np.timedelta64(1, 'Y')
-        self.df['release_very_new'] = (weeks_delta < 2)
-        self.df['release_very_old'] = (years_delta > 50)
-        self.df.drop(['release_date'], axis=1, inplace=True)
+    def __process_er_pr(self):
+        pass
 
-    def __prepare_homepage(self):
-        self.df['homepage'] = (~(self.df['homepage'].isna()) & (self.df['homepage'].str.len() > 0)).astype(int)
-
-    def __prepare_runtime(self):
-        counts = self.df.groupby('runtime')['runtime'].transform('size')
-        self.df['runtime_big'] = (counts > 170).astype(int)
-        self.df['runtime_medium'] = ((60 < counts) & (counts < 170)).astype(int)
-        self.df['runtime_small'] = ((0 < counts) & (counts < 60)).astype(int)
-        self.df.drop(['runtime'], axis=1, inplace=True)
-
-    def __prepare_crew(self):
-        mlb = MultiLabelBinarizer()
-        crew_list = lambda x: to_names_list(x, Preprocessing.KNOWN_CREW)
-        crew_encoding = pd.DataFrame(mlb.fit_transform(self.df['crew'].apply(crew_list)),
-                                     columns=mlb.classes_, index=self.df.index)
-        crew_encoding = add_missing_encodings(crew_encoding, Preprocessing.KNOWN_CREW)
-        self.df = self.df.join(crew_encoding)
-
-    def __prepare_cast(self):
-        mlb = MultiLabelBinarizer()
-        cast_list = lambda x: to_names_list(x, Preprocessing.KNOWN_CAST)
-        cast_encoding = pd.DataFrame(mlb.fit_transform(self.df['cast'].apply(cast_list)),
-                                     columns=mlb.classes_, index=self.df.index)
-        cast_encoding = add_missing_encodings(cast_encoding, Preprocessing.KNOWN_CAST)
-        self.df = self.df.join(cast_encoding)
+    def __process_actual_activity(self):
+        pass
 
 
 if __name__ == '__main__':
+
     pre = Preprocessing('Data and Supplementary Material-20220601/Mission 2 - Breast Cancer/train.feats.csv')
+
+    # show data with pandas profiling
+    src = 'Data and Supplementary Material-20220601/Mission 2 - Breast Cancer/train.feats.csv'
+    df = pd.read_csv(src, header=0)
+    prof = ProfileReport(df)
+    prof.to_file(output_file='output.html')
+
